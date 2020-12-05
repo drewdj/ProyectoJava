1733ddc1e371c06fc1b461ca143fa3a0d38bf48d
drewdj <43813535+drewdj@users.noreply.github.com>
2020-12-05 15:14:57 +0100

    Actualizacion gestor

---
+++ ProyectoFinal/src/GestorPrueba.java
@@ -0,0 +1,11 @@
+public class GestorPrueba {
+    public static void main(String[] args) {
+        Personaje personaje1 = new Personaje();
+
+        personaje1.setNombre("Jorge");
+        personaje1.setLocalizacionActual("Comedor");
+        personaje1.setLocalizacionObjetivo("Jardin");
+        personaje1.setObjetoActual("Pan");
+        personaje1.setObjetoObjetivo("Cartera");
+    }
+}
--- ProyectoFinal/src/Personaje.java
+++ ProyectoFinal/src/Personaje.java
@@ -5,4 +5,22 @@ public class Personaje {
     String objetoObjetivo;
     String localizacionActual;
     String localizacionObjetivo;
+
+    public void setNombre(String c){
+        this.nombre = c;
+    }
+    public void setObjetoActual(String c){
+        this.objetoActual = c;
+    }
+    public void setObjetoObjetivo(String c){
+        this.objetoObjetivo = c;
+    }
+    public void setLocalizacionActual(String c){
+        this.localizacionActual = c;
+    }
+    public void setLocalizacionObjetivo(String c){
+        this.localizacionObjetivo = c;
+    }
+
+
 }
