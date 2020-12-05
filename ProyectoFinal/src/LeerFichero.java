00f80f97a77ba93938a292974a5e4859057a8c0e
Swazer0110 <swazer0110@gmail.com>
2020-12-05 18:09:37 +0100

    File reader basico
    Pa los files

--- ProyectoFinal/src/LeerFichero.java
+++ ProyectoFinal/src/LeerFichero.java
@@ -1,5 +1,33 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+
+
 public class LeerFichero {
     public static void main(String[] args) {
 
+        
+    try {
+
+      File myObj = new File("prueba.txt");
+      Scanner myReader = new Scanner(myObj);
+
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+
+      myReader.close();
+
+    } 
+    
+    catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
+
+    }
+    
+
 }
