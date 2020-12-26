b1de41c7100ed82cef0db23551530f02e24d0f3a
Swazer0110 <swazer0110@gmail.com>
2020-12-26 17:18:43 +0100

    Prueba
    aaa

--- ProyectoFinal/src/LeerFichero.java
+++ ProyectoFinal/src/LeerFichero.java
@@ -5,17 +5,21 @@ import java.util.Scanner; // Import the Scanner class to read text files
 
 
 public class LeerFichero {
-    public static void main(String[] args) {
 
-        
+  public static void main(String[] args) {
+    
+    String datas="";
+    String Localizaciones="",Personajes="",Objetos="";
+    
+
     try {
 
-      File myObj = new File("prueba.txt");
+      File myObj = new File("ProyectoFinal\\src\\configuracion.txt");
       Scanner myReader = new Scanner(myObj);
 
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
-        System.out.println(data);
+        datas = datas + data;
       }
 
       myReader.close();
@@ -27,7 +31,15 @@ public class LeerFichero {
       e.printStackTrace();
     }
 
-    }
+    String[] partes = datas.split("<");
+    Localizaciones = partes[1];
+    Personajes = partes[2];
+    Objetos = partes[3];
+
+    System.out.println(Localizaciones);
+    System.out.println(Personajes);
+    System.out.println(Objetos);
+  }
     
 
 }
