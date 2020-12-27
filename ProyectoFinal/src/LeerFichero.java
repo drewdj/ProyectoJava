eb7046b4f863d6464093a0c02346b13e883bb4ca
Swazer0110 <swazer0110@gmail.com>
2020-12-27 18:55:46 +0100

    Update LeerFichero.java

--- ProyectoFinal/src/LeerFichero.java
+++ ProyectoFinal/src/LeerFichero.java
@@ -6,15 +6,21 @@ import java.util.Scanner; // Import the Scanner class to read text files
 
 public class LeerFichero {
 
+  String localizacionesString[];
+  String personajesString[];
+  String objetosString[];
+
+
   public static void main(String[] args) {
-    
+
+    //leer fichero de configuracion 
     String datas="";
-    String Localizaciones="",Personajes="",Objetos="";
+    String[] Localizaciones,Personajes,Objetos;
     
 
     try {
-
-      File myObj = new File("E:\\ProyectoJava\\ProyectoFinal\\src\\configuracion.txt");
+      
+      File myObj = new File("ProyectoFinal\\src\\configuracion.txt");
       Scanner myReader = new Scanner(myObj);
 
       while (myReader.hasNextLine()) {
@@ -32,14 +38,110 @@ public class LeerFichero {
     }
 
     String[] partes = datas.split("<");
-    Localizaciones = partes[1];
-    Personajes = partes[2];
-    Objetos = partes[3];
 
-    System.out.println(Localizaciones);
-    System.out.println(Personajes);
-    System.out.println(Objetos);
-  }
+    int[] tamanios= new int[3];
+    tamanios[0]=partes[1].length();
+    tamanios[1]=partes[2].length();
+    tamanios[2]=partes[3].length();
+
+    String[][] datos = new String[3][10];
+
+    int flag=0;
+    int flag2=0;
+    for(int i=0 ; i<3 ; i++){
+
+      datos[i][0]="";
+
+      for(int j=0,f=0; j<tamanios[i];j++){
+
+        if(flag==0){
+          switch(i){
+
+            case 0: 
+            j = 15;
+            break;
+
+            case 1:
+            j = 11;
+            break;
+
+            case 2:
+            j= 8;
+            break;
+
+          }
+          flag=1;
+        }
+
+        if(partes[i+1].charAt(j)==')'){
+          flag2++;
+          
+        }
+
+        datos[i][f]=datos[i][f]+partes[i+1].charAt(j);
+
+        if(flag2==1){
+          flag2=0;
+          f++;
+          datos[i][f]="";
+        }
+
+      }
+      
+      flag=0;
+    }
+
+    Localizaciones = datos[0];
+    Personajes = datos[1];
+    Objetos = datos[2];
     
+    //leer fichero de objetivos
+    /*
+    datas="";
+    try {
+
+      File myObj = new File("ProyectoFinal\\src\\objetivos.txt");
+      Scanner myReader = new Scanner(myObj);
+
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        datas = datas + data;
+      }
+      System.out.println(datas);
+      myReader.close();
+
+    } 
+    
+    catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+
+    */
+  }
+
+  public void setLocalizaString(String[] localizaString) {
+    this.localizacionesString = localizaString;
+  }
+
+  public void setObjetosString(String[] objetosString) {
+    this.objetosString = objetosString;
+  }
+
+  public void setPersonajesString(String[] personajesString) {
+    this.personajesString = personajesString;
+  }
+
+  public String[] getLocalizaString() {
+    return localizacionesString;
+  }
+
+  public String[] getObjetosString() {
+    return objetosString;
+  }
+
+  public String[] getPersonajesString() {
+    return personajesString;
+  }
 
 }
