f13027a999c47097d0ac5124480bca9ccb263265
Hao Long <longeliu1333@gmail.com>
2021-01-04 00:37:42 +0100

    numeros de personajes Presentes

--- ProyectoFinal/src/Localizacion.java
+++ ProyectoFinal/src/Localizacion.java
@@ -94,6 +94,7 @@ public class Localizacion {
     }
 
     public int getNumPersonajePresente() {
-        return numPersonajePresente;
+        int numPersonajePresente = personajesPresentes.size();
+    	return numPersonajePresente;
     }
 }
