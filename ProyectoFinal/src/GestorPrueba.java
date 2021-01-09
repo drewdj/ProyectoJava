545394adb9dd19fa0c28e416b0a3299dd8a27077
Hao Long <longeliu1333@gmail.com>
2021-01-09 22:37:39 +0100

    Coger Objeto en beta

--- ProyectoFinal/src/GestorPrueba.java
+++ ProyectoFinal/src/GestorPrueba.java
@@ -311,6 +311,9 @@ public class GestorPrueba{
 							if (arrayPersonajesOrdenado[turno].getQuienPide().equals(arrayPersonajes[i].getNombre())){
 								arrayPersonajesOrdenado[turno].darObjeto(arrayPersonajes[i]);
 							}
+							if (arrayPersonajesOrdenado[turno].getQuienPide().equals(jugador.getNombre())){
+                                arrayPersonajesOrdenado[turno].darObjeto(jugador);
+                            }
 						}
 
 					}
